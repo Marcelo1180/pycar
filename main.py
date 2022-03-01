9184375658dd4138bc8ce982c03a7566e2319fa9
Marcelo Arteaga <arteagamarcelo@gmail.com>
2022-03-01 19:21:14 -0400

    correct controls added

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+import json
 from machine import Pin, PWM
 from utils.wifi import Wifi
 import utils.ws as ws
 from utils.dcmotor import DCMotor
 
+# Constants
+MOTOR_FREQUENCY = 15000
+MOTOR_SPEED = 50
 # Pin definitions
-frequency = 15000
-# led = Pin(4, Pin.OUT)
 motor1_pin1 = Pin(5, Pin.OUT)
 motor1_pin2 = Pin(16, Pin.OUT)
-motor1_enable = PWM(Pin(14), frequency)
+motor1_enable = PWM(Pin(14), MOTOR_FREQUENCY)
 motor2_pin1 = Pin(0, Pin.OUT)
 motor2_pin2 = Pin(4, Pin.OUT)
-motor2_enable = PWM(Pin(12), frequency)
+motor2_enable = PWM(Pin(12), MOTOR_FREQUENCY)
 # Motor definition
 dc_motor1 = DCMotor(motor1_pin1, motor1_pin2, motor1_enable, 350, 1023)
 dc_motor2 = DCMotor(motor2_pin1, motor2_pin2, motor2_enable, 350, 1023)
@@ -23,49 +25,37 @@ ws.routefile("/", "public/index.html")
 
 @ws.route("/turn_left")
 def turn_left(query=None):
-    dc_motor2.stop()
-    dc_motor1.forward(50)
-    return "off"
+    dc_motor1.backward(MOTOR_SPEED)
+    dc_motor2.forward(MOTOR_SPEED)
+    return json.dumps({"action": "turn_left", "message": "OK"})
 
 
 @ws.route("/turn_right")
 def turn_right(query=None):
-    dc_motor1.stop()
-    dc_motor2.forward(50)
-    return "off"
+    dc_motor2.backward(MOTOR_SPEED)
+    dc_motor1.forward(MOTOR_SPEED)
+    return json.dumps({"action": "turn_right", "message": "OK"})
 
 
 @ws.route("/forward")
 def forward(query=None):
-    dc_motor1.forward(50)
-    dc_motor2.forward(50)
-    return "off"
+    dc_motor1.forward(MOTOR_SPEED)
+    dc_motor2.forward(MOTOR_SPEED)
+    return json.dumps({"action": "forward", "message": "OK"})
 
 
 @ws.route("/backward")
 def backward(query=None):
-    dc_motor1.backward(50)
-    dc_motor2.backward(50)
-    return "off"
+    dc_motor1.backward(MOTOR_SPEED)
+    dc_motor2.backward(MOTOR_SPEED)
+    return json.dumps({"action": "backward", "message": "OK"})
 
 
 @ws.route("/stop")
 def stop(query=None):
     dc_motor1.stop()
     dc_motor2.stop()
-    return "off"
-
-
-@ws.route("/off")
-def off(query=None):
-    led.off()
-    return "off"
-
-
-@ws.route("/on")
-def on(query=None):
-    led.on()
-    return "on"
+    return json.dumps({"action": "stop", "message": "OK"})
 
 
 ws.serve()
