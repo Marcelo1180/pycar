39a6f622e34a8c61d8434425fc878c89b4e7a9fe
Marcelo Arteaga <arteagamarcelo@gmail.com>
2022-03-01 15:16:34 -0400

    adding dc motor controls

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
+from machine import Pin, PWM
 from utils.wifi import Wifi
 import utils.ws as ws
-from machine import Pin
+from utils.dcmotor import DCMotor
 
 # Pin definitions
-led = Pin(4, Pin.OUT)
+frequency = 15000
+# led = Pin(4, Pin.OUT)
+pin1 = Pin(5, Pin.OUT)
+pin2 = Pin(4, Pin.OUT)
+enable = PWM(Pin(13), frequency)
+# Motor definition
+dc_motor = DCMotor(pin1, pin2, enable, 350, 1023)
 # Connecting to Wifi
 Wifi.connect()
 # Public a html file
 ws.routefile("/", "public/index.html")
+
+
+@ws.route("/forward")
+def forward(query=None):
+    dc_motor.forward(50)
+    return "off"
+
+
+@ws.route("/backward")
+def backward(query=None):
+    dc_motor.backward(50)
+    return "off"
+
+
+@ws.route("/stop")
+def stop(query=None):
+    dc_motor.stop()
+    return "off"
 
 
 @ws.route("/off")
